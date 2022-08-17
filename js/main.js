81c7a0f136cd99977e7f57172d748e8a91e1ed31
louy999 <louyhany999@gmail.com>
2022-08-17 09:04:21 +0200

    complete html & javasript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-var i = 5;
+var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+var emailInput = document.querySelector('input[type="email"]');
+var submit = document.querySelector('input[type="submit"]');
+submit.addEventListener("click", function () {
+    if (emailInput.value == "") {
+        emailInput.placeholder = "please enter your email";
+        setTimeout(function () {
+            emailInput.placeholder = "email@example.com";
+        }, 5000);
+    }
+    else {
+        if (emailInput.value.match(re)) {
+            console.log("yas");
+        }
+        else {
+            console.log("no");
+        }
+    }
+});
--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,23 @@
-let i = 5;
+const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+let emailInput = document.querySelector(
+  'input[type="email"]'
+) as HTMLInputElement;
+let submit = document.querySelector('input[type="submit"]') as HTMLInputElement;
+
+submit.addEventListener("click", () => {
+  if (emailInput.value == "") {
+    emailInput.placeholder = "please enter your email";
+
+    setTimeout(() => {
+      emailInput.placeholder = "email@example.com";
+    }, 5000);
+  } else {
+    if (emailInput.value.match(re)) {
+      console.log("yas");
+    } else {
+      console.log("no");
+    }
+  }
+});
